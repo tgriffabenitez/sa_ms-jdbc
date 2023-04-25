0f7e8aea0d2e44074ac92ab9de56826a5a452fe9
tgriffabenitez <tgriffabenitez@gmail.com>
2023-04-25 12:14:07 -0300

    inicio del proyecto

---
+++ ms-jdbc/src/main/java/com/sistemasactivos/msjdbc/MsJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.sistemasactivos.msjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsJdbcApplication.class, args);
+    }
+
+}
---
+++ ms-jdbc/src/test/java/com/sistemasactivos/msjdbc/MsJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemasactivos.msjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
