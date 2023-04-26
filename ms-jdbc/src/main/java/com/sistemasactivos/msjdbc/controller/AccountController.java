59110c3642cc35aeab3f372663b9c7a658a7916d
tgriffabenitez <tgriffabenitez@gmail.com>
2023-04-26 13:04:31 -0300

    Agregue el CrossOrigin y el RequestMapping con la uri api/v1

--- ms-jdbc/src/main/java/com/sistemasactivos/msjdbc/controller/AccountController.java
+++ ms-jdbc/src/main/java/com/sistemasactivos/msjdbc/controller/AccountController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/v1")
 public class AccountController {
 
     @Autowired
